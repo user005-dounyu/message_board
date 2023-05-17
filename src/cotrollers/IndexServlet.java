05b55e5274f2b91c0c7c414465ef7fe9fe2731a3
User <User@192.168.0.180>
2023-05-17 10:30:51 +0900

    IndexServlet.javaを作成

---
+++ src/cotrollers/IndexServlet.java
@@ -0,0 +1,23 @@
+package cotrollers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public IndexServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+}
